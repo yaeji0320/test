ce4e2d730b6e35172f52faceef0c2fe8f42530c7
yjl32 <60594367+yjl32@users.noreply.github.com>
2020-02-04 08:51:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name                = 'piptest',
+    name                = 'yjpip',
     version             = '0.1',
     description         = 'pip test',
     author              = 'yaeji',
