307109eb7d9a60d23e502f202b38608fd5373d1d
yjl32 <60594367+yjl32@users.noreply.github.com>
2020-02-04 08:17:16 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'piptest',
+    version             = '0.1',
+    description         = 'pip test',
+    author              = 'yaeji
+    author_email        = 'dldpwl0320@gmail.com',
+    url                 = 'https://github.com/yjl32/test',
+    download_url        = 'https://github.com/jeakwon/ccpy/archive/0.0.tar.gz',
+    install_requires    =  [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['piptest'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
